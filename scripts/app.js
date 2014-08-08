9928a4485c1caf2f00a469d9a3920a67d33018f9
shellfy <shellfy.g@gmail.com>
2014-08-08 16:53:04 +0800

    注释掉了不用的页面

--- scripts/app.js
+++ scripts/app.js
@@ -6,57 +6,59 @@
         redirectTo: '/dashboard'
       }).when('/dashboard', {
         templateUrl: 'views/dashboard.html'
-      }).when('/ui/typography', {
-        templateUrl: 'views/ui/typography.html'
-      }).when('/ui/buttons', {
-        templateUrl: 'views/ui/buttons.html'
-      }).when('/ui/icons', {
-        templateUrl: 'views/ui/icons.html'
-      }).when('/ui/grids', {
-        templateUrl: 'views/ui/grids.html'
-      }).when('/ui/widgets', {
-        templateUrl: 'views/ui/widgets.html'
-      }).when('/ui/components', {
-        templateUrl: 'views/ui/components.html'
-      }).when('/ui/timeline', {
-        templateUrl: 'views/ui/timeline.html'
-      }).when('/ui/nested-lists', {
-        templateUrl: 'views/ui/nested-lists.html'
-      }).when('/ui/pricing-tables', {
-        templateUrl: 'views/ui/pricing-tables.html'
-      }).when('/forms/elements', {
-        templateUrl: 'views/forms/elements.html'
-      }).when('/forms/layouts', {
-        templateUrl: 'views/forms/layouts.html'
-      }).when('/forms/validation', {
-        templateUrl: 'views/forms/validation.html'
-      }).when('/forms/wizard', {
-        templateUrl: 'views/forms/wizard.html'
-      }).when('/maps/gmap', {
-        templateUrl: 'views/maps/gmap.html'
-      }).when('/maps/jqvmap', {
-        templateUrl: 'views/maps/jqvmap.html'
-      }).when('/tables/static', {
-        templateUrl: 'views/tables/static.html'
-      }).when('/tables/responsive', {
-        templateUrl: 'views/tables/responsive.html'
-      }).when('/tables/dynamic', {
-        templateUrl: 'views/tables/dynamic.html'
-      }).when('/charts/others', {
-        templateUrl: 'views/charts/charts.html'
-      }).when('/charts/morris', {
-        templateUrl: 'views/charts/morris.html'
-      }).when('/charts/flot', {
-        templateUrl: 'views/charts/flot.html'
-      }).when('/mail/inbox', {
-        templateUrl: 'views/mail/inbox.html'
-      }).when('/mail/compose', {
-        templateUrl: 'views/mail/compose.html'
-      }).when('/mail/single', {
-        templateUrl: 'views/mail/single.html'
-      }).when('/pages/features', {
-        templateUrl: 'views/pages/features.html'
-      }).when('/pages/signin', {
+      })
+      // .when('/ui/typography', {
+      //   templateUrl: 'views/ui/typography.html'
+      // }).when('/ui/buttons', {
+      //   templateUrl: 'views/ui/buttons.html'
+      // }).when('/ui/icons', {
+      //   templateUrl: 'views/ui/icons.html'
+      // }).when('/ui/grids', {
+      //   templateUrl: 'views/ui/grids.html'
+      // }).when('/ui/widgets', {
+      //   templateUrl: 'views/ui/widgets.html'
+      // }).when('/ui/components', {
+      //   templateUrl: 'views/ui/components.html'
+      // }).when('/ui/timeline', {
+      //   templateUrl: 'views/ui/timeline.html'
+      // }).when('/ui/nested-lists', {
+      //   templateUrl: 'views/ui/nested-lists.html'
+      // }).when('/ui/pricing-tables', {
+      //   templateUrl: 'views/ui/pricing-tables.html'
+      // }).when('/forms/elements', {
+      //   templateUrl: 'views/forms/elements.html'
+      // }).when('/forms/layouts', {
+      //   templateUrl: 'views/forms/layouts.html'
+      // }).when('/forms/validation', {
+      //   templateUrl: 'views/forms/validation.html'
+      // }).when('/forms/wizard', {
+      //   templateUrl: 'views/forms/wizard.html'
+      // }).when('/maps/gmap', {
+      //   templateUrl: 'views/maps/gmap.html'
+      // }).when('/maps/jqvmap', {
+      //   templateUrl: 'views/maps/jqvmap.html'
+      // }).when('/tables/static', {
+      //   templateUrl: 'views/tables/static.html'
+      // }).when('/tables/responsive', {
+      //   templateUrl: 'views/tables/responsive.html'
+      // }).when('/tables/dynamic', {
+      //   templateUrl: 'views/tables/dynamic.html'
+      // }).when('/charts/others', {
+      //   templateUrl: 'views/charts/charts.html'
+      // }).when('/charts/morris', {
+      //   templateUrl: 'views/charts/morris.html'
+      // }).when('/charts/flot', {
+      //   templateUrl: 'views/charts/flot.html'
+      // }).when('/mail/inbox', {
+      //   templateUrl: 'views/mail/inbox.html'
+      // }).when('/mail/compose', {
+      //   templateUrl: 'views/mail/compose.html'
+      // }).when('/mail/single', {
+      //   templateUrl: 'views/mail/single.html'
+      // }).when('/pages/features', {
+      //   templateUrl: 'views/pages/features.html'
+      // })
+      .when('/pages/signin', {
         templateUrl: 'views/pages/signin.html'
       }).when('/pages/signup', {
         templateUrl: 'views/pages/signup.html'
@@ -66,23 +68,25 @@
         templateUrl: 'views/pages/lock-screen.html'
       }).when('/pages/profile', {
         templateUrl: 'views/pages/profile.html'
-      }).when('/404', {
-        templateUrl: 'views/pages/404.html'
-      }).when('/pages/500', {
-        templateUrl: 'views/pages/500.html'
-      }).when('/pages/blank', {
-        templateUrl: 'views/pages/blank.html'
-      }).when('/pages/invoice', {
-        templateUrl: 'views/pages/invoice.html'
-      }).when('/pages/services', {
-        templateUrl: 'views/pages/services.html'
-      }).when('/pages/about', {
-        templateUrl: 'views/pages/about.html'
-      }).when('/pages/contact', {
-        templateUrl: 'views/pages/contact.html'
-      }).when('/tasks', {
-        templateUrl: 'views/tasks/tasks.html'
-      }).otherwise({
+      })
+      // .when('/404', {
+      //   templateUrl: 'views/pages/404.html'
+      // }).when('/pages/500', {
+      //   templateUrl: 'views/pages/500.html'
+      // }).when('/pages/blank', {
+      //   templateUrl: 'views/pages/blank.html'
+      // }).when('/pages/invoice', {
+      //   templateUrl: 'views/pages/invoice.html'
+      // }).when('/pages/services', {
+      //   templateUrl: 'views/pages/services.html'
+      // }).when('/pages/about', {
+      //   templateUrl: 'views/pages/about.html'
+      // }).when('/pages/contact', {
+      //   templateUrl: 'views/pages/contact.html'
+      // }).when('/tasks', {
+      //   templateUrl: 'views/tasks/tasks.html'
+      // })
+      .otherwise({
         redirectTo: '/404'
       });
     }
--- scripts/shared/main.js
+++ scripts/shared/main.js
@@ -8,7 +8,7 @@
         return _.contains(['/404', '/pages/500', '/pages/login', '/pages/signin', '/pages/signin1', '/pages/signin2', '/pages/signup', '/pages/signup1', '/pages/signup2', '/pages/forgot', '/pages/lock-screen'], path);
       };
       return $scope.main = {
-        brand: 'Square',
+        brand: '微信平台',
         name: 'Lisa Doe'
       };
     }
