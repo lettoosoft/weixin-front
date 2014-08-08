8dfff16104f07cd606128c5fe34b34a986c60a18
Jeffrey Hu <zhiwehu@gmail.com>
2014-08-08 20:49:50 +0800

    use angular-cookies and logout

--- scripts/shared/main.js
+++ scripts/shared/main.js
@@ -1,24 +1,53 @@
 (function () {
     'use strict';
-    angular.module('app.controllers', ['app.services'])
+    angular.module('app.controllers', [])
         .controller('AppCtrl', [
-            '$scope', '$location', function ($scope, $location) {
+            '$scope', '$location', '$rootScope', 'LoginService', '$cookies', function ($scope, $location, $rootScope, LoginService, $cookies) {
+                $rootScope.apiHost = 'http://121.40.126.220';
+                $rootScope.user = null;
+
+                $rootScope.$on('event:auth-loginRequired', function (e, rejection) {
+                    /*delete $cookies.is_login;
+                     delete $cookies.authorization;
+                     AuthenticationService.saveAttemptUrl();
+                     $location.path('/access/signin');
+                     //$state.go('access.signin', {}, {reload: true, inherit: false});*/
+                    console.log('login required');
+                    $location.path('/pages/signin');
+                });
+
+                $rootScope.$on('event:auth-loginConfirmed', function () {
+                    console.log("loginConfirmed");
+                    $location.path('/dashboard');
+                    //AuthenticationService.redirectToAttemptedUrl();
+                    //$state.go('app.dashboard', {}, {reload: true, inherit: false});
+                });
+
+                $rootScope.$on('event:auth-logout-complete', function () {
+                    delete $cookies.is_login;
+                    delete $cookies.authorization;
+                    $location.path('/pages/signin');
+                });
+
                 $scope.isSpecificPage = function () {
                     var path;
                     path = $location.path();
                     return _.contains(['/404', '/pages/500', '/pages/login', '/pages/signin', '/pages/signin1', '/pages/signin2', '/pages/signup', '/pages/signup1', '/pages/signup2', '/pages/forgot', '/pages/lock-screen'], path);
                 };
+
+                $rootScope.logout = function(){
+                    LoginService.logout();
+                };
+
                 return $scope.main = {
                     brand: '微信平台',
                     name: 'Lisa Doe'
                 };
-
-
             }
         ])
 
         .controller('LoginCtrl', [
-            '$scope', 'LoginService',  function ($scope, LoginService) {
+            '$scope', 'LoginService', function ($scope, LoginService) {
                 $scope.login = function (user) {
                     LoginService.login(user);
                 }
@@ -39,9 +68,6 @@
         ])
 
         .controller('DashboardCtrl', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
-            $rootScope.apiHost = 'http://121.40.126.220';
-            $rootScope.user = null;
-
             var url = $rootScope.apiHost + '/api/v1/me/';
             $http.post(url)
                 .success(function (data, status, headers, config) {
@@ -50,32 +76,6 @@
                 .error(function (data, status, headers, config) {
                     console.log("Error occurred.  Status:" + status);
                 });
-
-            $rootScope.$on('event:auth-loginRequired', function (e, rejection) {
-                /*delete $cookies.is_login;
-                 delete $cookies.authorization;
-                 AuthenticationService.saveAttemptUrl();
-                 $location.path('/access/signin');
-                 //$state.go('access.signin', {}, {reload: true, inherit: false});*/
-                console.log('login required');
-                $location.path('/pages/signin');
-            });
-
-            $rootScope.$on('event:auth-loginConfirmed', function () {
-                console.log("loginConfirmed");
-                $location.path('/dashboard');
-                //AuthenticationService.redirectToAttemptedUrl();
-                //$state.go('app.dashboard', {}, {reload: true, inherit: false});
-            });
-
-            $rootScope.$on('event:auth-logout-complete', function () {
-                //delete $cookies.is_login;
-                //delete $cookies.authorization;
-                //$location.path('/access/signin');
-            });
-
         }]);
 
 }).call(this);
-
-//# sourceMappingURL=main.js.map
--- scripts/shared/service.js
+++ scripts/shared/service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app.services', [])
-    .factory('LoginService', function ($rootScope, $http, authService) {
+    .factory('LoginService', function ($rootScope, $http, authService, $cookies, $location, redirectToUrlAfterLogin) {
         var service = {
             login: function (user) {
                 var url = $rootScope.apiHost + '/api/v1/user/login/';
@@ -10,6 +10,10 @@ angular.module('app.services', [])
                         $rootScope.user = data;
                         var auth = 'ApiKey ' + data.username + ':' + data.apikey;
                         $http.defaults.headers.common.Authorization = auth;  // Step 1
+
+                        // Save auth apikey in cookie
+                        $cookies.is_login = true;
+                        $cookies.authorization = auth;
 
                         // Need to inform the http-auth-interceptor that
                         // the user has logged in successfully.  To do this, we pass in a function that
@@ -24,6 +28,34 @@ angular.module('app.services', [])
                     .error(function (data, status, headers, config) {
                         $rootScope.$broadcast('event:auth-login-failed', status);
                     });
+            },
+            logout: function (user) {
+                var url = $rootScope.apiHost + '/v1/user/logout/';
+                $http.post(url, {}, { ignoreAuthModule: true })
+                    .finally(function (data) {
+                        delete $http.defaults.headers.common.Authorization;
+                        $rootScope.$broadcast('event:auth-logout-complete');
+                    });
+            },
+            loginCancelled: function () {
+                authService.loginCancelled();
+            },
+            isLoggedIn: function () {
+                if ($cookies.is_login) {
+                    $http.defaults.headers.common.Authorization = $cookies.authorization;
+                    return true;
+                }
+                return false;
+            },
+            saveAttemptUrl: function () {
+                if ($location.path().toLowerCase() != '/pages/signin') {
+                    redirectToUrlAfterLogin.url = $location.path();
+                }
+                else
+                    redirectToUrlAfterLogin.url = '/dashboard';
+            },
+            redirectToAttemptedUrl: function () {
+                $location.path(redirectToUrlAfterLogin.url);
             }
         };
         return service;
