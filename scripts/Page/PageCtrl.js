9cacd72e6e719eb834d26ab237af9a6333d5f1b1
chenhanxiao <575090833@qq.com>
2014-08-19 16:09:06 +0800

    完善微信增加功能

--- scripts/Page/PageCtrl.js
+++ scripts/Page/PageCtrl.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 (function() {
   'use strict';
   angular.module('app.page.ctrls', []).controller('invoiceCtrl', [
@@ -20,8 +20,13 @@
     $scope.edit=0;
     $scope.tab=0;
     $scope.way=1;
-    $scope.weixin=weixin.select();
+    weixin.select($scope);
     $scope.add=function(addItem){
+      if($scope.weixin_num>=3){
+         alert("普通用户最多只能添加三个微信号！");
+         return;
+      }
+         
       $scope.weixin=weixin.add($scope.weixin,addItem);
     }
     $scope.Update=function(profile){
@@ -31,36 +36,7 @@
       $scope.tab=n;
     }
   }]);
-=======
-(function () {
-    'use strict';
-    angular.module('app.page.ctrls', []).controller('invoiceCtrl', [
-        '$scope', '$window', function ($scope, $window) {
-            return $scope.printInvoice = function () {
-                var originalContents, popupWin, printContents;
-                printContents = document.getElementById('invoice').innerHTML;
-                originalContents = document.body.innerHTML;
-                popupWin = window.open();
-                popupWin.document.open();
-                popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="styles/main.css" /></head><body onload="window.print()">' + printContents + '</html>');
-                return popupWin.document.close();
-            };
-        }
 
-    ]);
-    angular.module('app.page.ctrls', []).controller('profileCtrl', [ '$scope', 'UpdateUser', 'weixin', '$rootScope', function ($scope, UpdateUser, weixin, $rootScope) {
-        $scope.message = "123";
-        $scope.edit = 0;
-        $scope.tab = 0;
-        weixin.select($scope);
-        $scope.Update = function (profile) {
-            UpdateUser.Update(profile, $scope);
-        };
-        $scope.cTab = function (n) {
-            $scope.tab = n;
-        }
-    }]);
->>>>>>> c7426223117433e2f2d3879cfc6585133ce1d31c
 
 
 }).call(this);
