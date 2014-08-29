2056162836100091b98e5aec3c8f04c9ba693ee0
chenhanxiao <575090833@qq.com>
2014-08-29 11:09:21 +0800

    添加了上传头像功能

--- scripts/shared/main.js
+++ scripts/shared/main.js
@@ -1,6 +1,34 @@
 (function () {
     'use strict';
-    angular.module('app.controllers', [])   
+    var isOnGitHub = window.location.hostname === 'blueimp.github.io',
+        url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'http://121.40.126.220/api/v1/user/avatarupload/';      
+    angular.module('app.controllers', ['blueimp.fileupload']) 
+        .config([
+            '$httpProvider', 'fileUploadProvider',
+            function ($httpProvider, fileUploadProvider) {
+                delete $httpProvider.defaults.headers.common['X-Requested-With'];
+                fileUploadProvider.defaults.redirect = window.location.href.replace(
+                    /\/[^\/]*$/,
+                    '/cors/result.html?%s'
+                );
+                if (isOnGitHub) {
+                    // Demo settings:
+                    angular.extend(fileUploadProvider.defaults, {
+                        // Enable image resizing, except for Android and Opera,
+                        // which actually support image resizing, but fail to
+                        // send Blob objects via XHR requests:
+                        disableImageResize: /Android(?!.*Chrome)|Opera/
+                            .test(window.navigator.userAgent),
+                        maxFileSize: 5000000,
+                        previewMaxWidth: 300,
+                        previewMaxHeight: 280,                        
+                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+                    });
+                }
+            }
+        ])
+
+
         .controller('AppCtrl', [
             '$scope', '$location', '$rootScope', 'LoginService', '$cookies', function ($scope, $location, $rootScope, LoginService, $cookies) {
 
@@ -162,6 +190,62 @@
                 });
             }
         ])
+        .controller('DemoFileUploadController', [
+            '$scope', '$http', '$filter', '$window',
+            function ($scope, $http) {
+                $scope.options = {
+                    url: url
+                };
+
+                if (!isOnGitHub) {
+                    $scope.loadingFiles = true;
+                    $http.get(url)
+                        .then(
+                            function (response) {
+                                $scope.loadingFiles = false;
+                                //$scope.queue = response.data.files || [];
+                            },
+                            function () {
+                                $scope.loadingFiles = false;
+                            }
+                        );
+                }
+            }
+        ])
+
+        .controller('FileDestroyController', [
+            '$scope', '$http',
+            function ($scope, $http) {
+                var file = $scope.file,
+                    state;
+                    console.log(file.$response());
+                if (file.url) {
+
+                    file.$state = function () {
+                        return state;
+                    };
+                    file.$destroy = function () {
+                        state = 'pending';
+                        return $http({
+                            url: file.deleteUrl,
+                            method: file.deleteType
+                        }).then(
+                            function () {
+                                state = 'resolved';
+                                $scope.clear(file);
+                            },
+                            function () {
+                                state = 'rejected';
+                            }
+                        );
+                    };
+                } else if (!file.$cancel && !file._index) {
+                    file.$cancel = function () {
+                        $scope.clear(file);
+                    };
+                }
+            }
+        ])        
 
         .controller('DashboardCtrl', ['$scope', '$rootScope', '$http', '$location', function ($scope, $rootScope, $http, $location) {
             var url = $rootScope.apiHost + '/api/v1/me/';
